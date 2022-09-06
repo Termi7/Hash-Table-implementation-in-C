ed2beaa24cac0d226981a7f569ba2d6cc50d1029
Termi7 <90416021+Termi7@users.noreply.github.com>
2022-09-06 04:27:12 -0400

    progress 2

--- errors.h
+++ errors.h
@@ -3,12 +3,61 @@
 void displayError(int error, char* errorInfo);
 
 enum{
-  // 1 :"Error this not"
-  // 2:
-
-}
+  MissingInput ='File Not Found error message',
+  fileNotFound = 'File Not Found error message',
+  blankRecord = 'Blank Line Found',
+  corruptRecord ='Corrupt Record',
+  shortRecord='Corrupt Record',
+  duplicateName='Duplicate Name error message', //fix here
+  nameLength='Student Name Not Between 5 and 20' ,
+  numberLength='Student Number Not 9 Characters',
+  numberFormat='Student Number Is Invalid',
+  emailLength ='Student Email Not 17 Characters',
+  emailFormat='Student Email Format Is Invalid',
+  creditsValue='Student Credit Not Less Than 150' ,
+  GPAValue='Student GPA Not Less Than'
+};
 
 void displayError(int error, char* errorInfo)
 {
-	
-}
+	if(error==1){
+    printf("Error: File Not Found error message\n");
+  }
+  else if(error==2){
+    printf("Error: %s (%s)\n",corruptRecord, errorInfo);
+  }
+  else if(error ==4){
+    printf("Error: %s (%s)\n",nameLength, errorInfo);
+  }
+  else if(error==5){
+    printf("Error: %s (%s)\n",numberLength, errorInfo);
+  }
+  else if(error =6){
+    printf("Error: %s (%s)\n",numberFormat, errorInfo);
+  }
+  else if(error==7){
+
+    printf("Error: %s (%s)\n",emailLength, errorInfo);
+  }
+  else if(error==8){
+    printf("Error: %s (%s)\n",emailFormat, errorInfo);
+  }
+  else if(error ==9){
+    printf("Error: %s (%s)\n",creditsValue, errorInfo);
+  }
+ else if(error==10){
+  printf("Error: %s (%s)\n",GPAValue, errorInfo);
+ }
+  else if(error==11){
+  printf("Error: %s)\n",duplicateName, errorInfo);
+ }
+  
+
+ 
+
+  
+  
+
+  }
+  
+  
--- hashtable.h
+++ hashtable.h
@@ -31,6 +31,7 @@ struct student
 int computerHash(char* input)
 {
 	int hashValue=0;
+	int i = 0;
 	while(input[i]!='\0'){
   //  printf("\nASCII Value of %c = %d", str[i], str[i]);
 	 hashValue = input[i]+ hashValue;
@@ -42,33 +43,122 @@ int computerHash(char* input)
 
 void displayHashTable(struct student* hashTable[])
 {
+	printf("Hash Table Log\n");
+	printf("--------------\n");
+	// printf("Added Student to Hash Table: %s        Target: %d, Actual: %d", hashTable[].name, target, actual)
 	
 }
 
 void displayRecord(struct student* temp, int index)
 {
-
+	printf("Hash Table Contents\n");
+	printf("-----------------\n");
+	printf(' ');
+	printf("Index  Name                  Number     Email Address      Credits  GPA");
+	printf("-----  --------------------  ---------  -----------------  -------  ---");
+  printf("%d  %s          %s  %s       %d  %d", index, temp->name, temp->number, temp->email, temp->credits, temp->gpa);
+	
 }
 
 void initializeTable(struct student* hashTable[])
 {
-
+	
+	 for(int i=0;i<TABLE_SIZE; i++){
+        strcpy(hashTable[i]->name, NULL);
+				strcpy(hashTable[i]->email, NULL);
+				strcpy(hashTable[i]->credits, NULL);
+				hashTable[i]->credits = 0;
+				hashTable[i]->gpa = 0.0;
+				// hashTable[i]->next = NULL;
+    }
 }
 
 void insertStudent(struct student* hashTable[], struct student* temp)
 {
 	// Do not forget to use the malloc() function to reserve space for the student structure
 	// Do not forget to use the strcpy() function to store the string data in the hash table
-	// computerHash(&temp.name);
+	
+	//int * ptrMemomry ;
+
+	 int key;
+	 key =computerHash(temp->name);
+
+	
+
+	struct student *ptrMemory = (struct student*)malloc(TABLE_SIZE * sizeof(struct student));
+
+		strcpy(ptrMemory->name, temp->name);
+		strcpy(ptrMemory->name, temp->name);
+		strcpy(ptrMemory->name, temp->name);
+		ptrMemory->credits=temp->credits;
+		ptrMemory->gpa=temp->gpa;
+
+		int target=key;
+		
+		if(hashTable[key]!= NULL){
+			while(strcmp (hashTable[key]->name, temp->name)==0){
+				
+				key = key +1;
+				
+
+
+			}
+			int actual = key;
+			hashTable[key] =ptrMemory;
+
+
+		}
+
+
+
+    // newNode->next = NULL;
+	
 
 }
 
 bool testEmailFormat(struct student temp)
 {
 
+
+
 }
 
 bool testStudentData(struct student temp)
 {
+	if(strlen(temp.name)>=5 || strlen(temp.name)<=20 ){
+
+		return true;
+	}else{
+		return false;
+	}
+
+  if(strlen(temp.number)==9 &&temp.number[0]=='u'){
+		return true;
+	}else{
+
+		return false;
+	}
+
+	if(strlen(temp.email)==17 && testEmailFormat(temp)){
+
+		return true;
+	}else{
+
+		return false;
+	}
+	if(temp.credits>=0 ||temp.credits<=150 ){
+
+		return true;
+	}else{
+		return false;
+	}
+
+	if(temp.gpa>=5 || temp.gpa<=20 ){
+
+		return true;
+	}else{
+		return false;
+	}
+
 	
 }
